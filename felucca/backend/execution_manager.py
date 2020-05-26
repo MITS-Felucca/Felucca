a18ce577fbf70e2133aa61679f59662c71ef26c9
moody6520 <moody637cs@gmail.com>
2020-05-26 11:48:44 -0400

    submit job in execution manager

--- felucca/backend/execution_manager.py
+++ felucca/backend/execution_manager.py
@@ -1,2 +1,32 @@
+
+import requests
+import sys
+
+from resource_manager import ResourceManager
+from job_manager import JobManager
+from common.singleton import Singleton
+
+
+CONTAINER_PORT = '5000'
+@Singleton
 class ExecutionManager(object):
-    pass
+
+    def __init__(self):
+        self.tasks = {}
+
+    def sumbit_task(self, task_id, command_line_input):
+        container_ip = self.tasks[task_id][1].ip
+        requests.post("http://%s:%s/task" % (container_ip, CONTAINER_PORT), data={"task_id": task_id,
+                                                                                  "command_line_input": command_line_input})
+
+    def save_result(self, task_id, status, stderr, stdout):
+        container = self.tasks[task_id][1]
+        container.kill()
+        output_path = self.tasks[task_id]["output_path"]
+        log_path = self.tasks[task_id]["log_path"]
+        ResourceManager().save_result(task_id, output_path, log_path, stdout, stderr, status)
+        JobManager().finish_task(task_id)
+        self.tasks.pop(task_id, None)
+
+# submit_task
+# save_result
--- felucca/backend/server.py
+++ felucca/backend/server.py
@@ -1,11 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+from execution_manager import ExecutionManager
 
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/result", methods=['POST'])
+def get_result():
+    status = request.form['status']
+    ExecutionManager().save_result(request.form['task_id'],
+                                   status,
+                                   request.form['stderr'],
+                                   None if status == 'Error' else request.form['stdout'])
+    return {'is_received': True}
+
+@app.route("/task/<task_id>", method=['GET'])
+def get_task(task_id):
+    return {'command_line_input': ExecutionManager().get_command_line_input(task_id)}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
