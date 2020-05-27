0f0adcbe1e0e924c3e24183f777f072c621e67d8
xkbt777 <sudil@andrew.cmu.edu>
2020-05-26 22:29:04 -0400

    modify container server communication, add dockerfile environment

--- tests/unit_test/container_server/test_server.py
+++ tests/unit_test/container_server/test_server.py
@@ -3,10 +3,21 @@ from flask import request
 app = Flask(__name__)
 
 
+@app.route("/task/<task_id>", methods=['GET'])
+def get_task(task_id):
+    if task_id == '1':
+        return {'command_line_input': 'ooanalyzer -j output.json -F facts -R results -f /test/oo.exe'}
+    elif task_id == '2':
+        return {'command_line_input': 'ooanalyzer -j output.json -F facts -R results -f /backend/oo.exe'}
+    elif task_id == '3':
+        return {'command_line_input': 1}
+    return 'task_id not found', 404
+
+
 @app.route("/result", methods=['POST'])
 def print_result():
     print(request.form)
-    return 'OK'
+    return 'OK', 200
 
 
 if __name__ == '__main__':
