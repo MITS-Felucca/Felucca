156b1db70aee597d34353197d47b947a4193076a
xkbt777 <sudil@andrew.cmu.edu>
2020-07-11 18:37:46 -0400

    add service file and start script.

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d "/var/lib/Felucca" ]; then
-    git clone https://github.com/MITS-Felucca/Felucca.git /var/lib
+    git clone https://github.com/MITS-Felucca/Felucca.git /var/lib/Felucca
 else
     git pull -C /var/lib/Felucca
 fi
