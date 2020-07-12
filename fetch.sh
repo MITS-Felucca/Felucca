52be81d222ec1430d5cae16953e52bbd44123713
xkbt777 <sudil@andrew.cmu.edu>
2020-07-11 20:05:30 -0400

    add service file and start script.

--- fetch.sh
+++ fetch.sh
@@ -3,6 +3,7 @@
 if [ ! -d "/var/tmp/Felucca" ]; then
     git clone https://github.com/MITS-Felucca/Felucca.git /var/tmp/Felucca
 else
+    git -C /var/tmp/Felucca checkout deployment
     git -C /var/tmp/Felucca pull
 fi
 
