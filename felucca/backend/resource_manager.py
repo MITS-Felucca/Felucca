e5e50108e09e528159d23fa7c36eed6ba56431ab
Guancheng Li <895168349@qq.com>
2020-07-12 20:10:08 -0400

    Force RM to use database "felucca"

--- felucca/backend/resource_manager.py
+++ felucca/backend/resource_manager.py
@@ -17,8 +17,8 @@ class ResourceManager(object):
     """
 
     def __init__(self, db_name="felucca"):
-        # self.db_name = "test"
-        self.db_name = db_name
+        self.db_name = "felucca"
+        # self.db_name = db_name
         self.db_manager = self.DatabaseManager(self.db_name)
 
     def setup(self):
